658eaacc5113bf6327b85e4c0f28647e70832ff2
Leonard Chan <lc599@tux64-14.cs.drexel.edu>
2016-08-11 11:43:22 -0400

    gotta increase that github score

---
+++ hw3/chat.h
@@ -0,0 +1,16 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+/**
+ * Header to contain shared stuff between files.
+ */
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define UNIVERSAL_KEY 1337
+
+#endif
---
+++ hw3/server.c
@@ -0,0 +1,9 @@
+#include "chat.h"
+
+
+int main(int argc, char* argv[]){
+    // Write to shared piece of memory known to clients for indicating
+    // the server is on.
+
+    return 0;
+}
